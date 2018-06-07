f5c065b4785f8779ac4e184bde55a8a3219af1fe
elton.santos <elton.santos@mv.com.br>
2018-06-07 16:17:23 -0300

    adicionado os testes unitários

--- src/app/cadastra-movimentacao/MockMovimentacaoDao.ts
+++ src/app/cadastra-movimentacao/MockMovimentacaoDao.ts
@@ -27,13 +27,97 @@ export class MockMovimentacaoDao {
     verificarMovimentacoesSaoDespesas() {
         let teste = true;
         this.movimentacoes.forEach(element => {
-            if (element.descricao = "Despesa") {
+            if (element.tipo != "Despesa" ) {
                 teste = false;
             }
         });
         
         return teste;
 
+    }
+
+    verificarMovimentacoesSaoRecebimentos(){
+        let teste = true;
+        this.movimentacoes.forEach(element => {
+            if (element.tipo != "Recebimentos" ) {
+                teste = false;                
+            }
+        });
+        
+        return teste;
+    }
+
+    verificaDescricaoNula(){
+        let teste = false;
+        this.movimentacoes.forEach(element => {
+            if(element.descricao == null){
+                teste = true;
+            }
+        }
+        );
+
+        return teste;
+    }
+
+    verificaTipoNulo(){
+        let teste = false;
+        this.movimentacoes.forEach(element => {
+            if(element.tipo == null){
+                teste = true;
+            }
+        }
+        );
+
+        return teste;
+    }
+
+        verificaDataLançamentoNula(){
+        let teste = false;
+        this.movimentacoes.forEach(element => {
+            if(element.dataLancamento == null){
+                teste = true;
+            }
+        }
+        );
+
+        return teste;
+    }
+
+    verificaDataVencimentoNula(){
+        let teste = false;
+        this.movimentacoes.forEach(element => {
+            if(element.dataVencimento == null){
+                teste = true;
+            }
+        }
+        );
+
+        return teste;
+    }
+
+    verificaCategoriaNula(){
+        let teste = false;
+        this.movimentacoes.forEach(element => {
+            if(element.categoria == null){
+                teste = true;
+            }
+        }
+        );
+
+        return teste;
+    }
+
+    
+    verificaValorNulo(){
+        let teste = false;
+        this.movimentacoes.forEach(element => {
+            if(element.valor == null){
+                teste = true;
+            }
+        }
+        );
+
+        return teste;
     }
 
 }
--- src/app/cadastra-movimentacao/cadastra-movimentacao.component.spec.ts
+++ src/app/cadastra-movimentacao/cadastra-movimentacao.component.spec.ts
@@ -81,7 +81,7 @@ it('Movimentações SÃO despesas', () => {
 
   let result = dao.verificarMovimentacoesSaoDespesas();
   
-  expect(result).toBeTruthy;
+  expect(result).toBe(true);
 });
 
 it('Movimentações NÃO são despesas', () => {
@@ -97,7 +97,7 @@ it('Movimentações NÃO são despesas', () => {
   mock1.valor = "500";
 
   mock2.descricao = "Água"
-  mock2.tipo = "Despesa";
+  mock2.tipo = "Recebimentos";
   mock2.dataLancamento = "20/10/2016";
   mock2.dataVencimento = "20/10/2018";
   mock2.categoria = "Contas";
@@ -107,11 +107,150 @@ it('Movimentações NÃO são despesas', () => {
 
   let result = dao.verificarMovimentacoesSaoDespesas();
   
-  expect(result).toBeFalsy;
+  expect(result).toBe(false);
+});
+
+it('Movimentações NÃO são recebimentos', () => {
+  let dao = new MockMovimentacaoDao();
+  let mock1 = new MockMovimentacao();
+  let mock2 = new MockMovimentacao();
+ 
+  mock1.descricao = "Luz"
+  mock1.tipo = "Despesa";
+  mock1.dataLancamento = "20/10/2016";
+  mock1.dataVencimento = "20/10/2018";
+  mock1.categoria = "Contas";
+  mock1.valor = "500";
+
+  mock2.descricao = "Água"
+  mock2.tipo = "Recebimentos";
+  mock2.dataLancamento = "20/10/2016";
+  mock2.dataVencimento = "20/10/2018";
+  mock2.categoria = "Contas";
+  mock2.valor = "700";
+
+  dao.movimentacoes = [mock1, mock2];
+
+  let result = dao.verificarMovimentacoesSaoRecebimentos();
+  
+  expect(result).toBe(false);
 });
 
 
 
+
+it('Movimentações são recebimentos', () => {
+  let dao = new MockMovimentacaoDao();
+  let mock1 = new MockMovimentacao();
+  let mock2 = new MockMovimentacao();
+ 
+  mock1.descricao = "Luz"
+  mock1.tipo = "Recebimentos";
+  mock1.dataLancamento = "20/10/2016";
+  mock1.dataVencimento = "20/10/2018";
+  mock1.categoria = "Contas";
+  mock1.valor = "500";
+
+  mock2.descricao = "Água"
+  mock2.tipo = "Recebimentos";
+  mock2.dataLancamento = "20/10/2016";
+  mock2.dataVencimento = "20/10/2018";
+  mock2.categoria = "Contas";
+  mock2.valor = "700";
+
+  dao.movimentacoes = [mock1, mock2];
+
+  let result = dao.verificarMovimentacoesSaoRecebimentos();
+  
+  expect(result).toBe(true);
 });
+
+it('Verifica a descrição nula', () => {
+  let dao = new MockMovimentacaoDao();
+  let mock1 = new MockMovimentacao();
+  
+  mock1.descricao = null;
+
+  dao.movimentacoes = [mock1];
+
+  let result = dao.verificaDescricaoNula();
+
+  expect(result).toBe(true);
+
+});
+
+it('Verifica o tipo nulo', () => {
+  let dao = new MockMovimentacaoDao();
+  let mock1 = new MockMovimentacao();
+  
+  mock1.tipo = null;
+
+  dao.movimentacoes = [mock1];
+
+  let result = dao.verificaTipoNulo();
+
+  expect(result).toBe(true);
+
+});
+
+
+it('Verifica a dataLançamento nula', () => {
+  let dao = new MockMovimentacaoDao();
+  let mock1 = new MockMovimentacao();
+  
+  mock1.dataLancamento = null;
+
+  dao.movimentacoes = [mock1];
+
+  let result = dao.verificaDataLançamentoNula();
+
+  expect(result).toBe(true);
+
+});
+
+it('Verifica a dataVencimento nula', () => {
+  let dao = new MockMovimentacaoDao();
+  let mock1 = new MockMovimentacao();
+  
+  mock1.dataVencimento = null;
+
+  dao.movimentacoes = [mock1];
+
+  let result = dao.verificaDataVencimentoNula();
+
+  expect(result).toBe(true);
+
+});
+
+it('Verifica a categoria nula', () => {
+  let dao = new MockMovimentacaoDao();
+  let mock1 = new MockMovimentacao();
+  
+  mock1.categoria = null;
+
+  dao.movimentacoes = [mock1];
+
+  let result = dao.verificaCategoriaNula();
+
+  expect(result).toBe(true);
+
+})
+
+it('Verifica o valor nulo', () => {
+  let dao = new MockMovimentacaoDao();
+  let mock1 = new MockMovimentacao();
+  
+  mock1.valor = null;
+
+  dao.movimentacoes = [mock1];
+
+  let result = dao.verificaValorNulo();
+
+  expect(result).toBe(true);
+
+})
+
+});
+
 
 
